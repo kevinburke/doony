9bef448133f7d9ceccd33536c2caf8ecfc50ab9a
Kevin Burke <kevin@twilio.com>
2013-09-28 13:56:48 -0700

    Get it working as a Chrome extension. Add support for multi-configuration builds

--- doony.js
+++ doony.js
@@ -33,17 +33,30 @@ jQuery(function($) {
         return path.match(/^\/job\/.*?\//) !== null;
     }
 
+    /**
+     * This is a little tricky because it needs to match either the homepage or
+     * a page with configuration. The configuration check is for an equals sign
+     * in the 3rd part of the URL
+     */
     var isJobHomepage = function(path) {
+        return path.match(/^\/job\/.*?\/(.*?=.*?\/)?$/) !== null;
+    }
+
+    var isRootHomepage = function(path) {
         return path.match(/^\/job\/.*?\/$/) !== null;
+    };
+
+    var getRootJobUrl = function(path) {
+        return path.match(/^\/job\/.*?\//)[0];
     }
 
     // note: this function assumes you're already on a job page
     var getJobUrl = function(path) {
-        return path.match(/^\/job\/.*?\//)[0];
+        return path.match(/^\/job\/.*?\/(.*?=.*?\/)?/)[0];
     };
 
-    var redirectToNewJobConsole = function(jobUrl, buildNumber) {
-        $.get(jobUrl + 'api/json?tree=builds[number]', function(data) {
+    var redirectForUrl = function(jobUrl, buildNumber) {
+        $.getJSON(jobUrl + 'api/json?tree=builds[number]', function(data) {
             for (var i = 0; i < data.builds.length; i++) {
                 var build = data.builds[i];
                 if (build.number === buildNumber) {
@@ -53,9 +66,27 @@ jQuery(function($) {
             // gone all the way through and it's not there, sleep for a minute
             // and try again.
             setTimeout(function() {
-                redirectToNewJobConsole(jobUrl, buildNumber);
+                redirectForUrl(jobUrl, buildNumber);
             }, 1000);
         });
+    }
+
+    var redirectToNewJobConsole = function(jobUrl, buildNumber) {
+        if (isRootHomepage(jobUrl)) {
+            $.getJSON(jobUrl + 'api/json?tree=activeConfigurations[name]', function(data) {
+                if (data !== "{}" && 'activeConfigurations' in data) {
+                    // If its a multi configuration, just pick the first one.
+                    // This works for us, might have to make this configurable
+                    // somehow.
+                    var downstreamUrl = jobUrl + data.activeConfigurations[0].name + '/';
+                    return redirectForUrl(downstreamUrl, buildNumber);
+                } else {
+                    return redirectForUrl(jobUrl, buildNumber);
+                }
+            });
+        } else {
+            return redirectForUrl(jobUrl, buildNumber);
+        }
     };
 
     var showButterBar = function(message) {
@@ -81,15 +112,60 @@ jQuery(function($) {
         });
     });
 
+    // build a callout
+    var getCallout = function(message, href) {
+        return "<div class='doony-callout doony-callout-info'><a " +
+            (href === null ? "" : "href='" + href + "'") + ">" + message +
+            "</a></div>"
+    }
+
+    // xxx combine this with the getCallout below
+    var updateConfiguration = function(jobUrl, name) {
+        $.getJSON(jobUrl + name + 'api/json?tree=lastBuild[number]', function(data) {
+            if (data.lastBuild !== null && 'number' in data.lastBuild) {
+                $("#matrix .model-link").each(function(idx, item) {
+                    if (item.getAttribute('href') === name) {
+                        var href = jobUrl + name + data.lastBuild.number + '/consoleFull';
+                        var message = "View console output for the latest test";
+                        $(item).next(".doony-callout").children("a").attr('href', href);
+                    }
+                });
+            }
+        });
+    };
+
+    if ($("#matrix").length) {
+        // for some stupid reason jenkins fetches this with ajax so we need to
+        // setinterval here to continue to retrieve it all the time
+        setInterval(function() {
+            var jobUrl = getJobUrl(window.location.pathname);
+            if ($("#matrix .doony-downstream-link").length) {
+                // already updated this matrix div
+                return;
+            }
+            $("#matrix .model-link").wrap("<div class='doony-downstream-link'>");
+            // Create the div, even though we don't have the HREF yet, so the
+            // UI looks consistent
+            $("#matrix .model-link").each(function(idx, item) {
+                var message = "View console output for the latest test";
+                $(item).after(getCallout(message, null));
+            });
+            $.getJSON(jobUrl + 'api/json?tree=activeConfigurations[name]', function(data) {
+                for (var i = 0; i < data.activeConfigurations.length; i++) {
+                    var config = data.activeConfigurations[i];
+                    updateConfiguration(jobUrl, config.name + '/');
+                }
+            });
+        }, 50);
+    }
+
     if (isJobHomepage(window.location.pathname)) {
         var jobUrl = getJobUrl(window.location.pathname);
-        $.get(jobUrl + 'api/json?tree=lastBuild[number]', function(data) {
-            console.log(data.lastBuild.number);
-            var message = "View console output for the latest test"
+        $.getJSON(jobUrl + 'api/json?tree=lastBuild[number]', function(data) {
+            var message = "View console output for the latest test";
             var href = jobUrl + data.lastBuild.number + '/consoleFull';
             var h2 = $("h2:contains('Permalinks')");
-            h2.after("<div class='doony-callout doony-callout-info'><a href='" +
-                href + "'>" + message + "</a></div>");
+            h2.after(getCallout(message, href));
         });
     }
 
@@ -98,7 +174,7 @@ jQuery(function($) {
         button.className = "btn btn-primary doony-build";
         button.innerHTML = "Build Now";
         $(button).click(function() {
-            var jobUrl = getJobUrl(window.location.pathname);
+            var jobUrl = getRootJobUrl(window.location.pathname);
             // The build post endpoint doesn't tell you the number of the next
             // build, so get it before we create a build.
             $.getJSON(jobUrl + 'api/json?depth=1&tree=nextBuildNumber,lastBuild[building]', function(data) {
@@ -110,15 +186,16 @@ jQuery(function($) {
                         message += " <a href='#' id='doony-clear-build'>Cancel the current build</a>";
                     }
                     showButterBar(message);
-                    redirectToNewJobConsole(jobUrl, data.nextBuildNumber);
+                    redirectToNewJobConsole(getJobUrl(window.location.pathname),
+                        data.nextBuildNumber);
                 });
             });
         });
 
         $(document).on('click', '#doony-clear-build', function(e) {
             e.preventDefault();
-            var jobUrl = getJobUrl(window.location.pathname);
-            $.get(jobUrl + 'api/json?tree=lastBuild[number]', function(data) {
+            var jobUrl = getRootJobUrl(window.location.pathname);
+            $.getJSON(jobUrl + 'api/json?tree=lastBuild[number]', function(data) {
                 $.post(jobUrl + data.lastBuild.number + '/stop');
             });
         });
