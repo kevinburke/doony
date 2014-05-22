2caf6f9576b9d3875b31ccfcf308b14d93259a09
Josh Montoute <josh@thinksquirrel.com>
2014-05-22 02:54:27 -0500

    Added replacements for aborted_anime.gif and aborted.png

--- doony.js
+++ doony.js
@@ -568,10 +568,12 @@
         replaceBouncingFloatyBall("img[src*='red_anime.gif']", '#d9534f');
         replaceBouncingFloatyBall("img[src*='blue_anime.gif']", green);
         replaceBouncingFloatyBall("img[src*='grey_anime.gif']", '#999');
+        replaceBouncingFloatyBall("img[src*='aborted_anime.gif']", '#999');
         replaceBouncingFloatyBall("img[src*='yellow_anime.gif']", '#f0ad4e');
     }, 10);
     setInterval(function() {
         replaceFloatyBall("img[src*='/grey.png']", "aborted");
+        replaceFloatyBall("img[src*='/aborted.png']", "aborted");
         replaceFloatyBall("img[src*='/blue.png']", "success");
         replaceFloatyBall("img[src*='/red.png']", "failure");
         replaceFloatyBall("img[src*='/yellow.png']", "warning");
