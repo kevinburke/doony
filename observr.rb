94178dc25297e7d2947d62092a0068767d989453
Kevin Burke <kev@inburke.com>
2014-07-24 12:32:55 -0700

    add VERSION to watched fields

--- observr.rb
+++ observr.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env observr
 
 watch('src/theme.js') { puts "\033[31mCompiling javascript\033[0m"; system("make js") }
+watch('VERSION') { puts "\033[31mCompiling javascript\033[0m"; system("make js") }
 watch('src/doony.scss') { puts "\033[31mCompiling css\033[0m"; system("make css") }
