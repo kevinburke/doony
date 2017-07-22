07737fa33a1939cbfdc2a3c270f64b13826f37f3
andrei_fedorov <afedorov@luc.edu>
2017-07-22 18:17:27 -0500

    updated testresult.js as per repository owner's comments

--- src/testresult.js
+++ src/testresult.js
@@ -8,8 +8,6 @@
  * For details see testresult.md
  */
 if ((window.location.pathname).includes("testReport")) {
-    // for debugging
-    console.log("Page found!");
 
     // this function is called once the page is loaded
     window.onload = function () {
@@ -61,36 +59,33 @@ if ((window.location.pathname).includes("testReport")) {
             }
         }
 
-        // for debugging
-        console.log(totalDuration, totalFail, totalSkip, totalPass, totalTotal);
+        createTotalRow(newCells, columns);
 
-        // create total row and its cells
-        for (var i = 0; i < columns; i++) {
-            newCells[i] = newRow.insertCell(i);
-            newCells[i].style.textAlign = window.getComputedStyle(tableStyle).textAlign;
-            newCells[i].style.fontWeight = "bold";
+        function createTotalRow(newCells, columns) {
+            for (var i = 0; i < columns; i++) {
+                newCells[i] = newRow.insertCell(i);
+                newCells[i].style.textAlign = window.getComputedStyle(tableStyle).textAlign;
+                newCells[i].style.fontWeight = "bold";
+            }
+            switch (columns) {
+                case 3:
+                    newCells[0].appendChild(document.createTextNode("Total"));
+                    newCells[0].style.textAlign = "";
+                    newCells[0].style = "text-align:left; font-weight: bold;";
+                    newCells[1].appendChild(document.createTextNode(totalDuration.toFixed(1).toString() + " sec"));
+                    break;
+                default:
+                    newCells[0].appendChild(document.createTextNode("Total"));
+                    newCells[0].style.textAlign = "";
+                    newCells[0].style = "text-align:left; font-weight: bold;";
+                    newCells[1].appendChild(document.createTextNode(totalDuration.toFixed(1).toString() + " sec"));
+                    newCells[2].appendChild(document.createTextNode(totalFail.toString()));
+                    newCells[4].appendChild(document.createTextNode(totalSkip.toString()));
+                    newCells[6].appendChild(document.createTextNode(totalPass.toString()));
+                    newCells[8].appendChild(document.createTextNode(totalTotal.toString()));
+                    newCells[9].appendChild(document.createTextNode(""));
+                    break;
+            }
         }
-
-        // write data to the total row
-        switch (columns) {
-            case 3:
-                newCells[0].appendChild(document.createTextNode("Total"));
-                newCells[0].style.textAlign = "";
-                newCells[0].style = "text-align:left; font-weight: bold;";
-                newCells[1].appendChild(document.createTextNode(totalDuration.toFixed(1).toString() + " sec"));
-                break;
-            default:
-                newCells[0].appendChild(document.createTextNode("Total"));
-                newCells[0].style.textAlign = "";
-                newCells[0].style = "text-align:left; font-weight: bold;";
-                newCells[1].appendChild(document.createTextNode(totalDuration.toFixed(1).toString() + " sec"));
-                newCells[2].appendChild(document.createTextNode(totalFail.toString()));
-                newCells[4].appendChild(document.createTextNode(totalSkip.toString()));
-                newCells[6].appendChild(document.createTextNode(totalPass.toString()));
-                newCells[8].appendChild(document.createTextNode(totalTotal.toString()));
-                newCells[9].appendChild(document.createTextNode(""));
-                break;
-        }
-
     }
 }
